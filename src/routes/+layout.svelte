7a6673ccaa7d0747d080e97bff3d0e27bdead1fa
Abhijith Sunder <abhijithsunder01@gmail.com>
2024-11-09 18:34:52 +0530

    Dark mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,38 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
+  import { Button } from '$lib/components/ui/button';
+  import Moon from 'svelte-radix/Moon.svelte';
+  import Sun from 'svelte-radix/Sun.svelte';
   import '../app.css';
   let { children } = $props();
+  let dark = $state(false);
+  if (browser) {
+    dark = localStorage.getItem('dark') === 'true';
+  }
+  $effect(() => {
+    if (!dark) {
+      document.getElementsByTagName('html')[0].classList.remove('dark');
+      browser && localStorage.setItem('dark', 'false');
+    } else {
+      document.getElementsByTagName('html')[0].classList.add('dark');
+      browser && localStorage.setItem('dark', 'true');
+    }
+  });
 </script>
 
 <div class="flex w-screen h-screen flex-col">
-  <div class="pl-4 pt-2 pb-4 shadow-md border">
+  <div class="flex flex-row justify-between p-4 shadow-md border">
     <h1 class="scroll-m-20 text-2xl sm:text-4xl font-extrabold tracking-tight lg:text-4xl">
       Timecapsule
     </h1>
+    <Button variant="ghost" on:click={() => (dark = !dark)}>
+      {#if dark}
+        <Sun class="text-foreground" />
+      {/if}
+      {#if !dark}
+        <Moon class="text-foreground" />
+      {/if}
+    </Button>
   </div>
   <div class="flex flex-1 overflow-auto">
     {@render children()}
