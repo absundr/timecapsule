0922f5d95acba702a0764d73e7a479c3d2419141
Abhijith Sunder <abhijithsunder01@gmail.com>
2024-11-12 17:43:22 +0530

    setup crontask

--- src/lib/server/capsules.ts
+++ src/lib/server/capsules.ts
@@ -41,3 +41,41 @@ WHERE user_id = ?1
   query.finalize();
   return capsules as Capsule[];
 }
+
+type CapsulesToSend = {
+  userId: string;
+  username: string;
+  email: string;
+  capsuleId: string;
+  title: string;
+  message: string;
+  picture: string;
+};
+
+export function getCapsulesToSend(): CapsulesToSend[] {
+  const query = db.query(`
+select user.id as userId, user.username, user.email, capsule.id as capsuleId, capsule.title, capsule.message, capsule.picture 
+from 
+  capsule 
+inner join 
+  user on user.id = capsule.user_id 
+where 
+  capsule.sent = false and 
+  capsule.send_on = date(current_timestamp)
+`);
+  const capsulesToSend = query.all();
+  query.finalize();
+  return capsulesToSend as CapsulesToSend[];
+}
+
+export function markSent(capsuleIds: string[]) {
+  const query = db.prepare(`
+update capsule
+  set sent = true
+  where id = ?1
+`);
+  for (const id of capsuleIds) {
+    query.run(id);
+  }
+  query.finalize();
+}
---
+++ src/scripts/cron.ts
@@ -0,0 +1,46 @@
+import { getCapsulesToSend, markSent } from '$lib/server/capsules';
+import { $ } from 'bun';
+import fs, { appendFileSync } from 'fs';
+import cron from 'node-cron';
+
+function log(text: string) {
+  appendFileSync('logs/sendlog.txt', text + '\n');
+}
+async function processAndSendTimecapsules() {
+  log(new Date().toUTCString());
+  log('Starting execution....');
+  const successIds: string[] = [];
+  const capsulesToSend = getCapsulesToSend();
+  log(`Total capsules to send: ${capsulesToSend.length}`);
+  for (const capsule of capsulesToSend) {
+    let args = `"${capsule.email}" "${capsule.title}" "${capsule.message}"`;
+    if (capsule.picture) {
+      args += ` ${process.env.PUBLIC_API_URL}/api/user/${capsule.userId}/image/${capsule.picture}`;
+    }
+    try {
+      if (process.platform === 'win32') {
+        await $`wsl -e bash src/scripts/capsule-mailer.sh ${args}`;
+      } else {
+        await $`bash src/scripts/capsule-mailer.sh ${args}`;
+      }
+      successIds.push(capsule.capsuleId);
+    } catch (e) {
+      log(`Failed sending capsule: ${capsule.capsuleId}\nError: ${e}`);
+    }
+  }
+  log(
+    `Total success: ${successIds.length}\nTotal failure: ${capsulesToSend.length - successIds.length}`,
+  );
+  log('Done');
+  log('-----------------------------------');
+  markSent(successIds);
+}
+
+if (!fs.existsSync('logs')) {
+  fs.mkdirSync('logs');
+}
+
+// Run every 3 hours
+cron.schedule('* * */3 * * *', async () => {
+  await processAndSendTimecapsules();
+});
