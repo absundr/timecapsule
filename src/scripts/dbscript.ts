e4e07cf6d0ef7e1dbe839626314ee34f11254b8a
Abhijith Sunder <abhijithsunder01@gmail.com>
2024-11-18 22:21:03 +0530

    update db script

---
+++ src/scripts/dbscript.ts
@@ -0,0 +1,50 @@
+import db from '$lib/server/db';
+
+db.exec(
+  `
+    CREATE TABLE user (
+    id TEXT NOT NULL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    created_on DATE NOT NULL DEFAULT current_timestamp,
+    verified BOOLEAN NOT NULL DEFAULT false
+)
+    `,
+);
+
+db.exec(
+  `
+    CREATE TABLE session (
+    id TEXT NOT NULL PRIMARY KEY,
+    user_id TEXT NOT NULL REFERENCES user(id),
+    expires_at INTEGER NOT NULL
+)
+    `,
+);
+
+db.exec(
+  `
+    CREATE TABLE capsule (
+    id TEXT NOT NULL PRIMARY KEY,
+    user_id TEXT NOT NULL REFERENCES user(id),
+    title TEXT NOT NULL CHECK(LENGTH(title) <= 100),
+    message TEXT NOT NULL CHECK(LENGTH(message) <= 1000),
+    picture VARCHAR(30), 
+    send_on DATE NOT NULL,
+    sent BOOLEAN NOT NULL,
+    created_on DATE NOT NULL DEFAULT current_timestamp
+)
+    `,
+);
+
+db.exec(
+  `
+    CREATE TABLE verification (
+    email VARCHAR(50) NOT NULL PRIMARY KEY REFERENCES user(email),
+    otp CHAR(4) NOT NULL,
+    created_on INTEGER NOT NULL,
+    expires_at INTEGER NOT NULL
+)
+    `,
+);
