f65750d0e536e153e5d58c0e27b2632114030b42
Abhijith Sunder <abhijithsunder01@gmail.com>
2024-11-26 23:37:21 +0530

    update deploy script

--- pm2.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Timecapsule', // Name of your application
-  script: './build', // Entry point of your application
-  interpreter: 'bun', // Bun interpreter
-  env: {
-    PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-  },
-};
